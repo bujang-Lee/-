ba9e2eb78bbcfd7632b2aeb592254405ca4ec216
doseobujang <doseobujang@naver.com>
2021-02-28 11:44:28 +0900

    Delete htdocs directory

--- htdocs/view.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_list():
-    files = os.listdir("data")
-    list_str = ""
-    for item in files:
-        list_str = list_str + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
-    return list_str
