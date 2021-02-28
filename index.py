e5684822acbd9eaffc3530938919be51058f3842
doseobujang <doseobujang@naver.com>
2021-02-28 12:23:46 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,15 +1,4 @@
 #!python
 print("content-type: text/html; charset=euc-kr")
 print()
-import cgi, os, view
-
-form = cgi.FieldStorage()
-
-print('''
-<!DOCTYPE html>
-<html>
-  <body>
-    Hi
-  </body>
-</html>
-''')
+print("HI")
